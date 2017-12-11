358e558ec6c9d9aeae08eb5e312bdfa699cf5509
bosses11020 <harit20451@outlook.com>
2017-12-11 23:43:26 +0700

    add abstract

---
+++ abstract-factory-pattern/AbstractFactory.java
@@ -0,0 +1,6 @@
+
+
+public abstract class AbstractFactory {
+    abstract Color getColor(String color);
+    abstract Shape getShape(String shape) ;
+}
---
+++ abstract-factory-pattern/Color.java
@@ -0,0 +1,5 @@
+
+
+public interface Color {
+    void fill();
+}
